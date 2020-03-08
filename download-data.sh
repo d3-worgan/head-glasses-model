f9f6cd0e571c86188bdefb46426802881f8b998a
d3-worgan <daniel2.worgan@live.uwe.ac.uk>
2020-03-08 23:17:30 +0000

    update downloader

--- download-data.sh
+++ download-data.sh
@@ -1,26 +1,27 @@
-# Prepare a server for downloading the open images dataset
+# Download data from the open images dataset
 
 # Set up python pip
 sudo curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 sudo apt-get install python3-distutils
 python3 get-pip.py 
-sudo nano ~/.bashrc
 
-# Add path to pip 
-"/home/danielworgan91/.local/bin"
-
-sudo reboot
-
-# On reboot...
 pip install tqdm
-
-cd /home/data-drive/download
-
 pip install awscli
 
+git clone https://github.com/d3-worgan/oi-detection-training.git
+cd oi-detection-training
 wget https://storage.googleapis.com/openimages/2018_04/class-descriptions-boxable.csv
 wget https://storage.googleapis.com/openimages/2018_04/train/train-annotations-bbox.csv
 wget https://storage.googleapis.com/openimages/2018_04/validation/validation-annotations-bbox.csv
 wget https://storage.googleapis.com/openimages/2018_04/test/test-annotations-bbox.csv
 
-python3 multhithread-downloader.py
+# Make sure classes .txt has correct classes
+python3 download-multi.py --mode test --classes classes.txt
+python3 download-multi.py --mode train --classes classes.txt
+
+python3 fix-yolo-annotations.py --classes classes.txt --location test
+python3 fix-yolo-annotations.py --classes classes.txt --location train
+
+
+
+
